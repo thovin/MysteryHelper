965e6f0537164616164d0bd8b832ef17c93dd70c
TJordan <Tjordan7@stu.jsu.edu>
2022-02-03 16:59:01 -0600

    finish project

--- app/src/main/java/com/example/mysteryhelper/MainActivity.java
+++ app/src/main/java/com/example/mysteryhelper/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.mysteryhelper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] options;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        options = getResources().getStringArray(R.array.textview);
+
+        findViewById(R.id.helpButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                displayRandOption();
+            }
+        });
+    }
+
+    private void displayRandOption() {
+        TextView optionDisplay = findViewById(R.id.contentText);
+
+        int optionIndex = ThreadLocalRandom.current().nextInt(0, options.length);
+
+        optionDisplay.setText(options[optionIndex]);
     }
 }
